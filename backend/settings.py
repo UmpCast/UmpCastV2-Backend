cc2e48f39dc47626d7e1a8713da2ac5a27489258
Jonathan Kao <jkao97@yahoo.com>
2020-07-16 20:50:06 -0700

    aws s3 file storage

--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'rest_framework_social_oauth2',
     'django_filters',
 
+    # amazon aws s3
+    'storages',
+
     'users',
     'leagues',
 ]
@@ -137,6 +140,17 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# Amazon AWS S3
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 # CUSTOM SETTINGS
 AUTH_USER_MODEL = 'users.User'
