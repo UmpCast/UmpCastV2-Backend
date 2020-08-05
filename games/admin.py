496272456b8f3381e3506ac05b92f5af51911c88
Jonathan Kao <jkao97@yahoo.com>
2020-08-04 20:17:38 -0700

    add games admin

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Game, Post, Application
 
-# Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'date_time', 'division', 'is_active', 'location', 'description')
+    list_display_links = ('pk', 'title')
+    search_fields = ('pk', 'title')
+    list_per_page = 25
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'game', 'role')
+    list_display_links = ('pk',)
+    list_per_page = 25
+
+
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'post', 'user', 'comments')
+    list_display_links = ('pk', )
+    list_per_page = 25
+
+
+admin.site.register(Game, GameAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Application, ApplicationAdmin)
