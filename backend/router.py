fc23754e4f2df9ffcb3aa6de00618872ab041717
Jonathan Kao <jkao97@yahoo.com>
2020-08-12 12:10:38 -0700

    add untested notifications viewsets, permissions, serializers, filters, routes

--- backend/router.py
+++ backend/router.py
@@ -1,6 +1,7 @@
 from users.api.viewsets import UserViewSet, UserLeagueStatusViewSet
 from leagues.api.viewsets import LeagueViewSet, RoleViewSet, DivisionViewSet, LevelViewSet
 from games.api.viewsets import GameViewSet, PostViewSet, ApplicationViewSet
+from notifications.api.viewsets import UmpCastNotificationViewSet, LeagueNotificationViewSet, GameNotificationViewSet, ApplicationNotificationViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -13,7 +14,10 @@ router.register('levels', LevelViewSet, basename='level')
 router.register('games', GameViewSet, basename='game')
 router.register('posts', PostViewSet, basename='post')
 router.register('applications', ApplicationViewSet, basename='application')
-
+router.register('ump-cast-notifications', UmpCastNotificationViewSet, basename='ump-cast-notification')
+router.register('league-notifications', LeagueNotificationViewSet, basename='league-notification')
+router.register('game-notifications', GameNotificationViewSet, basename='game-notification')
+router.register('application-notifications', ApplicationNotificationViewSet, basename='application-notification')
 
 for url in router.urls:
     print(url)
---
+++ notifications/api/filters.py
@@ -0,0 +1,42 @@
+from django_filters import rest_framework as filters
+from ..models import UmpCastNotification, LeagueNotification, GameNotification, ApplicationNotification
+from games.models import Application
+
+
+class UmpCastNotificationFilter(filters.FilterSet):
+    date_time = filters.IsoDateTimeFromToRangeFilter()
+
+    class Meta:
+        model = UmpCastNotification
+        fields = ['date_time']
+
+
+class LeagueNotificationFilter(filters.FilterSet):
+    date_time = filters.IsoDateTimeFromToRangeFilter()
+    user = filters.CharFilter(field_name='league__user', lookup_expr='pk')
+
+    class Meta:
+        model = LeagueNotification
+        fields = ['date_time']
+
+
+class GameNotificationFilter(filters.FilterSet):
+    date_time = filters.IsoDateTimeFromToRangeFilter()
+    user = filters.CharFilter(method='game_filter_by_user')
+
+    class Meta:
+        model = GameNotification
+        fields = ['date_time']
+
+    def game_filter_by_user(self, queryset, name, value):
+        game_ids = Application.objects.filter(user__pk=value).values_list('post__game__pk', flat=True)
+        return queryset.filter(game__pk__in=game_ids)
+
+
+class ApplicationNotificationFilter(filters.FilterSet):
+    date_time = filters.IsoDateTimeFromToRangeFilter()
+    user = filters.CharFilter(field_name='application__user', lookup_expr='pk')
+
+    class Meta:
+        model = ApplicationNotification
+        fields = ['date_time']
