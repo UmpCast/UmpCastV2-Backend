d9e43c0043c3c2d4030afc15d361fef9e4ddfcf1
Jonathan Kao <jkao97@yahoo.com>
2020-08-19 13:00:40 -0700

    Change TeamSnapNote structure, admin, migrations

--- teamsnap/models.py
+++ teamsnap/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils.timezone import now
-from django.contrib.postgres.fields import ArrayField
 
 
 class TeamSnapNote(models.Model):
-    notes = ArrayField(
-        models.CharField(max_length=128), size=64
-    )
     league = models.ForeignKey('leagues.League', on_delete=models.CASCADE)
     date_time = models.DateTimeField(default=now)
 
     class Meta:
         ordering = ['-date_time']
 
+
+class TeamSnapNoteItem(models.Model):
+    teamsnap_note = models.ForeignKey(TeamSnapNote, on_delete=models.CASCADE)
+    note = models.CharField(max_length=128)
