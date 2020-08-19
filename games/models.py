0b3008e51a1b44c665cd6044020a5300e3ed8932
Jonathan Kao <jkao97@yahoo.com>
2020-08-19 12:44:16 -0700

    Title, location to 128 chars. Game sync method

--- games/models.py
+++ games/models.py
@@ -1,15 +1,16 @@
 from django.db import models
-from ordered_model.models import OrderedModel
 from django.db.models.signals import post_save
+from ordered_model.models import OrderedModel
+
 from backend.mixins import OrderedModelUpdateMixin
 
 
 class Game(models.Model):
     division = models.ForeignKey('leagues.Division', on_delete=models.CASCADE)
-    title = models.CharField(max_length=32)
+    title = models.CharField(max_length=128)
     date_time = models.DateTimeField()
     is_active = models.BooleanField(default=True)
-    location = models.CharField(max_length=32)
+    location = models.CharField(max_length=128)
     description = models.TextField(blank=True, max_length=1028, null=True)
 
     # ts
@@ -17,6 +18,39 @@ class Game(models.Model):
 
     class Meta:
         ordering = ['-date_time']
+
+    def sync(self, **kwargs):
+        # expected fields: division, title, date_time, location, is_active
+        attrs = ['title', 'date_time', 'location', 'is_active']
+        update_fields = []
+        exception_notes = []
+
+        if self.division != kwargs['division']:
+            ts_id = self.ts_id
+            exception_notes.append(
+                f"{self.title} changed divisions on teamsnap. The original game has been deleted"
+            )
+            self.delete()
+            game = Game.objects.create(
+                division=kwargs['division'], title=kwargs['title'],
+                date_time=kwargs['date_time'], is_active=kwargs['is_active'],
+                location=kwargs['location'], ts_id=ts_id
+            )
+            exception_notes.append(
+                f"{self.title} has been created"
+            )
+            return exception_notes
+
+        for attr in attrs:
+            if getattr(self, attr) != kwargs[attr]:
+                update_fields.append(attr)
+                self.attr = kwargs[attr]
+        if update_fields:
+            self.save(update_fields=update_fields)
+            exception_notes.append(
+                f"{self.title} had the following items updated: {', '.join(update_fields)}"
+            )
+        return exception_notes
 
 
 class Post(models.Model):
@@ -29,6 +63,7 @@ def create_posts_from_game(sender, instance, *args, **kwargs):
     if kwargs['created']:
         for role in instance.division.role_set.all():
             Post.objects.create(game=instance, role=role)
+
 
 post_save.connect(create_posts_from_game, sender=Game)
 
