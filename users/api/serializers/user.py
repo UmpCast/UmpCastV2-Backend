1994b1ad03c7352a441510a40849d3a754e5f99f
Jonathan Kao <jkao97@yahoo.com>
2020-08-20 20:51:46 -0700

    user serializer includes accepted/pending/rejected league fields

--- users/api/serializers/user.py
+++ users/api/serializers/user.py
@@ -1,19 +1,25 @@
+import re
+
+import django.contrib.auth.password_validation as validators
 from rest_framework import serializers
 from rest_framework.serializers import ValidationError
+
 from users.models import User
-import django.contrib.auth.password_validation as validators
-import re
 
 
 class UserProfilePrivateBaseSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(max_length=128, write_only=True)
+    accepted_leagues = serializers.SerializerMethodField()
+    pending_leagues = serializers.SerializerMethodField()
+    rejected_leagues = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('pk', 'account_type', 'leagues', 'email', 'email_notifications',
+        fields = ('pk', 'account_type', 'email', 'email_notifications',
                   'first_name', 'last_name', 'phone_number', 'phone_notifications', 'profile_picture',
-                  'date_joined', 'password', 'password2')
-        read_only_fields = ('pk', 'leagues')
+                  'date_joined', 'password', 'password2', 'accepted_leagues', 'pending_leagues', 'rejected_leagues')
+        read_only_fields = ('pk', 'accepted_leagues',
+                            'pending_leagues', 'rejected_leagues')
         extra_kwargs = {
             'password': {'write_only': True},
         }
@@ -32,16 +38,28 @@ class UserProfilePrivateBaseSerializer(serializers.ModelSerializer):
             raise ValidationError('invalid phone_number length')
         pattern = re.compile("^([0-9]+)+$")
         if not pattern.match(phone_number):
-            raise ValidationError('phone numbers can only contain numeric values')
+            raise ValidationError(
+                'phone numbers can only contain numeric values')
         return phone_number
+
+    def get_accepted_leagues(self, instance):
+        return instance.leagues.accepted().values_list('pk', flat=True)
+
+    def get_pending_leagues(self, instance):
+        return instance.leagues.pending().values_list('pk', flat=True)
+
+    def get_rejected_leagues(self, instance):
+        return instance.leagues.rejected().values_list('pk', flat=True)
 
 
 class UserProfilePublicSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('pk', 'first_name', 'last_name', 'profile_picture', 'date_joined', 'account_type')
-        read_only_fields = ('pk', 'first_name', 'last_name', 'profile_picture', 'date_joined', 'account_type')
+        fields = ('pk', 'first_name', 'last_name',
+                  'profile_picture', 'date_joined', 'account_type')
+        read_only_fields = ('pk', 'first_name', 'last_name',
+                            'profile_picture', 'date_joined', 'account_type')
 
 
 class UserProfilePrivateCreateSerializer(UserProfilePrivateBaseSerializer):
@@ -67,7 +85,8 @@ class UserProfilePrivateUpdateSerializer(UserProfilePrivateBaseSerializer):
     def update(self, instance, validated_data):
         if 'password' in validated_data:
             if 'password2' not in validated_data:  # password2 must be passed in, validation in validate_password2
-                raise ValidationError("must provide password2 when updating password")
+                raise ValidationError(
+                    "must provide password2 when updating password")
             instance.set_password(validated_data.pop('password'))
         instance.save()
         return super().update(instance, validated_data)
