f476088541dcff4e2ec73b214c37dd84a004a555
Jonathan Kao <jkao97@yahoo.com>
2020-08-25 19:42:33 -0700

    Update ts save url name

--- teamsnap/urls.py
+++ teamsnap/urls.py
@@ -5,5 +5,5 @@ from .api.views import TeamSnapBuildView, TeamSnapSyncView, TeamSnapSaveKeyView
 urlpatterns = [
     path('<int:pk>/build/', TeamSnapBuildView.as_view(), name='teamsnap-build'),
     path('<int:pk>/sync/', TeamSnapSyncView.as_view(), name='teamsnap-sync'),
-    path('<int:pk>/save/', TeamSnapSaveKeyView.as_view(), name='teamsnap-test')
+    path('<int:pk>/save/', TeamSnapSaveKeyView.as_view(), name='teamsnap-save')
 ]
