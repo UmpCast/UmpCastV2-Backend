168c73aa835cd9bd92917294046476c3fff06760
Jonathan Kao <jkao97@yahoo.com>
2020-09-23 21:39:39 -0700

    filter applications using casted field

--- games/api/filters.py
+++ games/api/filters.py
@@ -29,7 +29,15 @@ class GameFilter(filters.FilterSet):
 class ApplicationFilter(filters.FilterSet):
     date_time = filters.IsoDateTimeFromToRangeFilter(
         field_name='post__game__date_time')
+    casted = filters.BooleanFilter(method='get_casted')
 
     class Meta:
         model = Application
         fields = ['user', 'date_time']
+
+    def get_casted(self, queryset, name, value):
+        app_ids = []
+        for app in Application.objects.all().iterator():
+            if app.is_casted() == value:
+                app_ids.append(app.id)
+        return Application.objects.filter(id__in=app_ids)
--- games/models.py
+++ games/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models.signals import post_save
 from ordered_model.models import OrderedModel
+from django.utils import timezone
+from users.models import UserLeagueStatus
 
 from backend.mixins import OrderedModelUpdateMixin
 
@@ -58,6 +60,47 @@ class Post(models.Model):
     role = models.ForeignKey('leagues.Role', on_delete=models.CASCADE)
     notes = models.TextField(blank=True, max_length=1028, null=True)
 
+    # def over_scheduling_limit(self, adv_scheduling):
+    #     return (self.game.date_time - timezone.now()).days > adv_scheduling
+
+    # def has_visibility(self, user):
+    #     return self.role in UserLeagueStatus.objects.get(user, league=self.game.division.league).visibilities.all()
+
+    # def check_apply_status(self, user):
+    #     adv_scheduling = self.game.division.league.adv_scheduling_limit
+    #     if self.over_scheduling_limit(adv_scheduling):
+    #         return {
+    #             'status': 'invalid',
+    #             'type': 'over_scheduling_limit',
+    #             'adv_scheduling_limit': str(adv_scheduling)
+    #         }
+    #     if self.game.division.league not in user.leagues.accepted():
+    #         return {
+    #             'status': 'invalid',
+    #             'type': 'league'
+    #         }
+    #     if not self.has_visibility(user):
+    #         return {
+    #             'status': 'invalid',
+    #             'type': 'visibility'
+    #         }
+    #     if self.application_set.filter(user=user).exists():
+    #         return {
+    #             'status': 'invalid',
+    #             'type': 'duplicate_post'
+    #         }
+    #     for post in self.game.post_set.all():
+    #         if post.application_set.filter(user=user).exists():
+    #             return {
+    #                 'status': 'invalid',
+    #                 'type': 'duplicate_game',
+    #                 'post': str(post.application_set.get(user=user).pk)
+    #             }
+    #     return {
+    #         'status': 'valid',
+    #         'type': 'success'
+    #     }
+
 
 def create_posts_from_game(sender, instance, *args, **kwargs):
     if kwargs['created']:
@@ -73,3 +116,6 @@ class Application(OrderedModelUpdateMixin, OrderedModel):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     comments = models.TextField(blank=True, max_length=1028, null=True)
     order_with_respect_to = 'post'
+
+    def is_casted(self):
+        return self.get_ordering_queryset().get_min_order() == self.order
